bc8da6e4d4b5640eb43a2ddfc19cb7c698822889
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-12 16:36:45 +0800

    Menghadiri pertemuan4

---
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-danger navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Rafli Blog</a>
+        <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" aria-current="{{ ($title === "Home") ? 'page' : '' }}" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" aria-current="{{ ($title === "About") ? 'page' : '' }}" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ ($title === "Posts") ? 'active' : '' }}" aria-current="{{ ($title === "Posts") ? 'page' : '' }}" href="/blog">Blog</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Rafli Afriansyah",
         "email" => "nashkihaysnairfailfardammahum@gmail.com",
         "image" => "suisei-lookalike.png"
@@ -26,5 +29,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rafli Afriansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vero, dolores similique accusantium consequatur facere veniam repellendus ipsam fugit suscipit quas est eligendi repellat placeat in voluptatibus minus molestias unde modi nihil eaque deserunt odio recusandae. Error nesciunt animi accusamus itaque vero enim ullam fugiat praesentium illo velit, id ducimus? Consequuntur accusantium quaerat est illo, consectetur earum aperiam natus. Quam eum asperiores repudiandae dolorum cupiditate natus voluptates animi officia est cumque quos accusantium eligendi reprehenderit ratione nam voluptatum, fuga facilis."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Haysnairfa Ilfar",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus error sapiente reiciendis saepe, eaque quisquam totam laboriosam adipisci nisi reprehenderit cumque tempora culpa aspernatur eligendi vero soluta illum nostrum quaerat voluptatum. Vero, vitae reiciendis quo non ratione fugit a esse libero, ad sit at quaerat quae. Optio maiores minima molestias repellendus dolores sapiente possimus quam id voluptates aperiam, architecto eius itaque beatae sunt molestiae suscipit sequi. Nihil, asperiores! Laudantium laboriosam a vero incidunt. Voluptate magnam quae fuga quod, ipsam necessitatibus accusamus animi sequi exercitationem alias consectetur dolor nam ut quas expedita eaque aperiam fugiat et debitis! Eligendi suscipit magni possimus!"
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('/posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rafli Afriansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vero, dolores similique accusantium consequatur facere veniam repellendus ipsam fugit suscipit quas est eligendi repellat placeat in voluptatibus minus molestias unde modi nihil eaque deserunt odio recusandae. Error nesciunt animi accusamus itaque vero enim ullam fugiat praesentium illo velit, id ducimus? Consequuntur accusantium quaerat est illo, consectetur earum aperiam natus. Quam eum asperiores repudiandae dolorum cupiditate natus voluptates animi officia est cumque quos accusantium eligendi reprehenderit ratione nam voluptatum, fuga facilis."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Haysnairfa Ilfar",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus error sapiente reiciendis saepe, eaque quisquam totam laboriosam adipisci nisi reprehenderit cumque tempora culpa aspernatur eligendi vero soluta illum nostrum quaerat voluptatum. Vero, vitae reiciendis quo non ratione fugit a esse libero, ad sit at quaerat quae. Optio maiores minima molestias repellendus dolores sapiente possimus quam id voluptates aperiam, architecto eius itaque beatae sunt molestiae suscipit sequi. Nihil, asperiores! Laudantium laboriosam a vero incidunt. Voluptate magnam quae fuga quod, ipsam necessitatibus accusamus animi sequi exercitationem alias consectetur dolor nam ut quas expedita eaque aperiam fugiat et debitis! Eligendi suscipit magni possimus!"
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
