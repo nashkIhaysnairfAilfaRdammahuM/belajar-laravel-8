78d80fb19cc9ae155c15e1faa36cdeb0a3381565
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-16 17:03:31 +0800

    Menghadiri pertemuan9

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -2,9 +2,9 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <p>By: Rafli Afriansyah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By: <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
 
-    <a href="/posts">Back to posts</a>
+    <a href="/posts" class="text-decoration-none">Back to posts</a>
 @endsection
--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -2,11 +2,10 @@
 @section('container')
     <h1>Halaman Blog Posts</h1>
     @foreach ($posts as $post)
-    <article class="mb-3">
-        <h2>
-            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-        </h2>
-        <p>{{ $post->excerpt }} <a href="/posts/{{ $post->slug }}">Read more...</a></p>
+    <article class="mb-3 border-bottom">
+        <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+        <p>By: <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+        <p>{{ $post->excerpt }} <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a></p>
     </article>
     @endforeach    
 @endsection
